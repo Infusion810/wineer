38f1f6adf43fbad64a507afcd00732a114675f36
Infusion810 <infusionpvtltd@gmail.com>
2025-03-30 02:09:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ const io = socketIo(server, {
 
 // app.use(
 //   cors({
-//     origin: ["https://dynexbet-front.vercel.app", "https://dynexbet-admin.vercel.app"], // Replace '*' with the specific origin(s) you want to allow, e.g., 'https://yourdomain.com'
+//     origin: ["https://winnerone.xyz", "https://admin.winnerone.xyz"], // Replace '*' with the specific origin(s) you want to allow, e.g., 'https://yourdomain.com'
 //     methods: ['POST', 'GET', 'PUT', 'DELETE'], // Define allowed HTTP methods
 //     credentials: true, // Allow credentials like cookies to be sent
 //   })
